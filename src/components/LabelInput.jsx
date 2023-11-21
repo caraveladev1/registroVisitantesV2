b1d389fa6690261ca8955f5482d57e44a5c6282f
Juan David <Juan.diaz@caravela.coffee>
2023-11-21 16:31:49 -0500

    cambios en el menu de contraseña

--- src/components/LabelInput.jsx
+++ src/components/LabelInput.jsx
@@ -6,12 +6,14 @@ export function LabelInput({
 	idLabel,
 	required,
 	value,
+	type
 }) {
 	return (
 		<input
 			placeholder={placeholder}
 			onChange={onChange}
 			id={idLabel}
+			type={type}
 			className="p-2 border-2 border-black rounded-2xl w-full focus:outline-none focus:border-sky"
 			required={required}
 			value={value}
