6c3a7d3b4162569f16a53d1b76f575ffd8f88fc5
caraveladev1 <80789516+caraveladev1@users.noreply.github.com>
2024-05-22 10:17:16 -0500

    add react-ga4 for traking

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+	HashRouter,
+	Navigate,
+	Route,
+	Routes,
+	useLocation,
+} from "react-router-dom";
 import "../i18n.js";
 import { AdminLogin } from "./Pages/AdminLogin";
 import { AdminPage } from "./Pages/AdminPage";
@@ -12,11 +18,25 @@ import { EmployeeForm } from "./Pages/EmployeeForm";
 import { TransportForm } from "./Pages/TransportForm";
 import { VisitorForm } from "./Pages/VisitorForm";
 import { PrivateRoute } from "./components/PrivateRoute";
+import ReactGA from "react-ga4";
 
 import "./index.css";
 
+ReactGA.initialize("G-01J5FZCB2Y");
+
+const TrackPageView = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		ReactGA.send({ hitType: "pageview", page: location.pathname });
+	}, [location]);
+
+	return null;
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<HashRouter basename="/">
+		<TrackPageView />
 		<Routes>
 			<Route path="/" element={<App />} />
 			<Route path="/EmployeeForm" element={<EmployeeForm />} />
